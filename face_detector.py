115a008cc2611ffe8c5ef5e6af95c0674c55fe1a
kc3222 <39278351+kc3222@users.noreply.github.com>
2020-02-22 13:46:57 -0500

    Update VideoCapture()

--- face_detector.py
+++ face_detector.py
@@ -3,7 +3,7 @@ import cv2
 import face_recognition
 
 # Get a reference to webcam 
-video_capture = cv2.VideoCapture("/dev/video1")
+video_capture = cv2.VideoCapture(0)
 
 # Initialize variables
 face_locations = []
