a5f72e3fcaedada0c408ffdeed6a038b7bc3ce5a
Quang Nhat Tran <trannhatquang1104@gmail.com>
2020-02-23 00:17:24 -0500

    add menu and buttons

--- quick_text.py
+++ quick_text.py
@@ -2,33 +2,79 @@ import cv2
 import os
 from PyQt5 import QtGui
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QAction, QFileDialog
 from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt, QPoint, QRect
 import sys
 import time
 
 PIC_DIR = "images"
+ICON_DIR = "icons"
 
-'''
-I need to draw a rect
-Click and drag
-Then get user input from openCV. Can I do so?
-'''
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-
-        self.label = QtWidgets.QLabel()
-        canvas = QtGui.QPixmap(800, 600)
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
+        self.setWindowTitle("Write a painting")
+        self.setGeometry(300, 300, 800, 600)
+        # self.label = QtWidgets.QLabel()
+        # self.canvas = QtGui.QPixmap(800, 600)
+        self.image = QImage(self.size(), QImage.Format_RGB32)
+        self.image.fill(Qt.white)
+        # self.label.setPixmap(self.canvas)
+        menubar = self.menuBar()
+        # self.setCentralWidget(self.label)
         self.time = time.time()
         self.selection_start = None
         self.selection_end = None
         self.obj_name = None
-        #self.draw_something()
+        self.doodling = True
+        # self.setWindowTitle("Write a painting")
+
+        # exitAction = QAction(QtGui.QIcon('cat.png'), 'Exit', self)
+        exitAction = QAction('Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.triggered.connect(self.close)
+
+        saveAction = QAction('Save', self)
+        saveAction.setShortcut('Ctrl+S')
+        saveAction.setStatusTip('Save image')
+        saveAction.triggered.connect(self.save_image)
+
+        doodleAction = QAction(QtGui.QIcon(os.path.join(ICON_DIR, "pencil_icon.png")), 'doodle', self)
+        doodleAction.triggered.connect(self.start_doodling)
+
+        writeAction = QAction(QtGui.QIcon(os.path.join(ICON_DIR, "text_icon.png")), 'write', self)
+        writeAction.triggered.connect(self.start_writing)
+
+
+        self.statusBar()
+
+
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        fileMenu.addAction(saveAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+
+
+        # self.setGeometry(300, 300, 350, 250)
+        self.show()
+
+    def start_doodling(self):
+        self.doodling = True
+
+    def start_writing(self):
+        self.doodling = False
+
+    def save_image(self):
+        # self.canvas.toImage().save("fire.png")
+        filePath, _ = QFileDialog.getSaveFileName(self, "Save image", "", "PNG(*.png);;JPEG(*.jpg *.jpeg);; All Files()")
+        if filePath == "":
+            return
+        self.image.save(filePath)
 
     def mousePressEvent(self, e):
         # if event.buttons() & QtCore.Qt.LeftButton:
@@ -55,11 +101,6 @@ class MainWindow(QtWidgets.QMainWindow):
         # self.insert_pic(pic_path, self.selection_start[0], self.selection_start[1], w, h)
 
 
-    def draw_something(self):
-        painter = QtGui.QPainter(self.label.pixmap())
-        painter.drawLine(10, 10, 300, 200)
-        painter.end()
-
     def getText(self):
         text, okPressed = QInputDialog.getText(self, "Get name", "Object's name:", QLineEdit.Normal, "")
         if okPressed:
@@ -67,7 +108,8 @@ class MainWindow(QtWidgets.QMainWindow):
             print(text)
 
     def insert_pic(self, pic_path, rect):
-        painter = QtGui.QPainter(self.label.pixmap())
+        # painter = QtGui.QPainter(self.label.pixmap())
+        painter = QtGui.QPainter(self.image)
         pic = QtGui.QPixmap(pic_path)
         painter.drawPixmap(rect, pic)
         # painter.drawPixmap(x,y,w,h,pic)
@@ -75,7 +117,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.update()
 
     def mouseMoveEvent(self, e):
-        painter = QtGui.QPainter(self.label.pixmap())
+        # painter = QtGui.QPainter(self.label.pixmap())
+        painter = QtGui.QPainter(self.image)
         pen = QtGui.QPen()
         pen.setWidth(10)
         pen.setColor(QtGui.QColor('red'))
@@ -87,6 +130,10 @@ class MainWindow(QtWidgets.QMainWindow):
                 f.write(f"{e.x()}, {e.y()}\n")
         painter.end()
         self.update()
+
+    def paintEvent(self, e):
+        canvasPainter = QPainter(self)
+        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
 
 def get_pic_by_name(name):
     name_dir = os.path.join(PIC_DIR, name)
