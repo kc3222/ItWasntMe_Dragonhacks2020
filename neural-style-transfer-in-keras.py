Add combine functions and fix scaling

neural-style-transfer-in-keras.py
@@ -12,9 +12,9 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 import os
-print(os.listdir("../input"))
-print(os.listdir("../input/style-transfer"))
-print(os.listdir("../input/deepdream"))
+# print(os.listdir("../input"))
+# print(os.listdir("../input/style-transfer"))
+# print(os.listdir("../input/deepdream"))
 # Any results you write to the current directory are saved as output.
 
 
@@ -37,45 +37,45 @@ from keras.models import Model
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.misc import imsave
 
-content_image=Image.open('../input/deepdream/Ireland.NorthernIReland.CarrickaRede.SS_.jpg')
-content_image=content_image.resize((512,512))
-style_image= Image.open('../input/style-transfer/style.png')
-style_image=style_image.resize((512,512))
+# content_image=Image.open('../input/deepdream/Ireland.NorthernIReland.CarrickaRede.SS_.jpg')
+# content_image=content_image.resize((512,512))
+# style_image= Image.open('../input/style-transfer/style.png')
+# style_image=style_image.resize((512,512))
 
-content_array=np.asarray(content_image,dtype='float32')
-content_array=np.expand_dims(content_array,axis=0)
+# content_array=np.asarray(content_image,dtype='float32')
+# content_array=np.expand_dims(content_array,axis=0)
 
-style_array=np.asarray(style_image,dtype='float32')
-style_array=np.expand_dims(style_array,axis=0)
+# style_array=np.asarray(style_image,dtype='float32')
+# style_array=np.expand_dims(style_array,axis=0)
 
-content_array[:, :, :, 0] -= 103.939
-content_array[:, :, :, 1] -= 116.779
-content_array[:, :, :, 2] -= 123.68
-content_array=content_array[:, :, :, ::-1]
+# content_array[:, :, :, 0] -= 103.939
+# content_array[:, :, :, 1] -= 116.779
+# content_array[:, :, :, 2] -= 123.68
+# content_array=content_array[:, :, :, ::-1]
 
-style_array[:, :, :, 0] -= 103.939
-style_array[:, :, :, 1] -= 116.779
-style_array[:, :, :, 2] -= 123.68
-style_array=style_array[:, :, :, ::-1]
-style_array.shape
+# style_array[:, :, :, 0] -= 103.939
+# style_array[:, :, :, 1] -= 116.779
+# style_array[:, :, :, 2] -= 123.68
+# style_array=style_array[:, :, :, ::-1]
+# style_array.shape
 
-height=512
-width=512
-content_image=backend.variable(content_array)
-style_image=backend.variable(style_array)
-combination_image=backend.placeholder((1,height,width,3))
+# height=512
+# width=512
+# content_image=backend.variable(content_array)
+# style_image=backend.variable(style_array)
+# combination_image=backend.placeholder((1,height,width,3))
 
-input_tensor=backend.concatenate([content_image,style_image,combination_image],axis=0)
+# input_tensor=backend.concatenate([content_image,style_image,combination_image],axis=0)
 
-model=VGG16(input_tensor=input_tensor,weights='imagenet', include_top=False)
+# model=VGG16(input_tensor=input_tensor,weights='imagenet', include_top=False)
 
-content_weight = 0.05
-style_weight = 5.0
-total_variation_weight = 1.0
+# content_weight = 0.05
+# style_weight = 5.0
+# total_variation_weight = 1.0
 
-layers=dict([(layer.name, layer.output) for layer in model.layers])
+# layers=dict([(layer.name, layer.output) for layer in model.layers])
 
-loss=backend.variable(0.)
+# loss=backend.variable(0.)
 
 def content_loss(content, combination):
     return backend.sum(backend.square(content-combination))
@@ -152,7 +152,7 @@ class Evaluator(object):
     
 evaluator=Evaluator()
 
-x=np.random.uniform(0,255,(1,height,width,3))-128.0
+# x=np.random.uniform(0,255,(1,height,width,3))-128.0
 
 iterations = 10
 
@@ -166,14 +166,14 @@ for i in range(iterations):
     end_time = time.time()
     print('Iteration %d completed in %ds' % (i, end_time - start_time))
     
-x = x.reshape((height, width, 3))
-x = x[:, :, ::-1]
-x[:, :, 0] += 103.939
-x[:, :, 1] += 116.779
-x[:, :, 2] += 123.68
-x = np.clip(x, 0, 255).astype('uint8')
+# x = x.reshape((height, width, 3))
+# x = x[:, :, ::-1]
+# x[:, :, 0] += 103.939
+# x[:, :, 1] += 116.779
+# x[:, :, 2] += 123.68
+# x = np.clip(x, 0, 255).astype('uint8')
 
-Image.fromarray(x)
+# Image.fromarray(x)
 
 
 # %%
@@ -181,8 +181,8 @@ Image.fromarray(x)
 """Defining initial variables"""
 from keras.preprocessing.image import load_img, img_to_array
 
-target_image_path = '../input/style-transfer/test_transfer.png'
-style_reference_image_path = '../input/style-transfer/style.png'
+target_image_path = './style-transfer/test_transfer.png'
+style_reference_image_path = './style-transfer/style.png'
 
 # Dimensions of the generated picture
 width, height = load_img(target_image_path).size

quick_draw.py
@@ -26,19 +26,19 @@ tf.set_random_seed(seed=42) # cài đặt seed
 # %%
 import ast
 
-# # owls = pd.read_csv('../input/quickdraw-doodle-recognition/train_simplified/owl.csv')
-# owls = pd.read_csv('./test.csv')
-# owls = pd.read_csv('./out_standard.csv')
-# # print(owls.columns)
-# # owls = owls[owls.recognized]
-# # owls['timestamp'] = pd.to_datetime(owls.timestamp)
-# # owls = owls.sort_values(by='timestamp', ascending=False)[-100:]
-# owls['drawing'] = owls['drawing'].apply(ast.literal_eval)
+# owls = pd.read_csv('../input/quickdraw-doodle-recognition/train_simplified/owl.csv')
+owls = pd.read_csv('./test.csv')
+owls = pd.read_csv('./out_standard.csv')
+# print(owls.columns)
+# owls = owls[owls.recognized]
+# owls['timestamp'] = pd.to_datetime(owls.timestamp)
+# owls = owls.sort_values(by='timestamp', ascending=False)[-100:]
+owls['drawing'] = owls['drawing'].apply(ast.literal_eval)
 
 owls.head()
 
 # %%
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # n = 2
 # fig, axs = plt.subplots(nrows=n, ncols=n, sharex=True, sharey=True, figsize=(16, 10))
@@ -109,22 +109,24 @@ def df_to_image_array_xd(df, size, lw=6, time_color=True):
     for i, raw_strokes in enumerate(df.drawing.values):
         temp = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
         x[i, :, :, 0] = temp
-        # plt.figure()
-        # plt.imshow(temp)
+        print('Hello')
+        plt.figure()
+        plt.imshow(temp)
+        plt.show()
     x = preprocess_input(x).astype(np.float32)
     return x
 
 # %%
-test_df = pd.read_csv(os.path.join("./out_standard.csv"))
-test_drawing = df_to_image_array_xd(test_df, 128)
-print(test_drawing.shape)
+# test_df = pd.read_csv(os.path.join("./out_standard.csv"))
+# test_drawing = df_to_image_array_xd(test_df, 128)
+# print(test_drawing.shape)
 
 # %%
 # print(test_drawing)
 
 # %%
-predictions = densenet_model.predict(test_drawing)
-print(predictions.shape)
+# predictions = densenet_model.predict(test_drawing)
+# print(predictions.shape)
 
 # %%
 def f2cat(filename: str) -> str:
@@ -150,7 +152,7 @@ class Simplified():
 s = Simplified('../quick-draw-doodle')
 NCSVS = 100
 categories = s.list_all_categories()
-print(len(categories))
+# print(len(categories))
 for i in range(len(categories)):
     categories[i] = categories[i].replace(' ', '_')
 
@@ -160,11 +162,21 @@ for i in range(len(categories)):
 #         print(i)
 
 # %%
-for i, prediction in enumerate(predictions):
-    top_3_predictions = prediction.argsort()[-3:][::-1]
-    # print(categories[top_3_predictions[0]] + ' ' + categories[top_3_predictions[1]] + ' ' + categories[top_3_predictions[2]])
-    print(categories[top_3_predictions[0]])
-    print(categories[top_3_predictions[1]])
-    print(categories[top_3_predictions[2]])
+# for i, prediction in enumerate(predictions):
+#     top_3_predictions = prediction.argsort()[-3:][::-1]
+#     # print(categories[top_3_predictions[0]] + ' ' + categories[top_3_predictions[1]] + ' ' + categories[top_3_predictions[2]])
+#     print(categories[top_3_predictions[0]])
+#     print(categories[top_3_predictions[1]])
+#     print(categories[top_3_predictions[2]])
 
 # %%
+def predict_image():
+    test_df = pd.read_csv(os.path.join("./out_standard.csv"))
+    test_drawing = df_to_image_array_xd(test_df, 128)
+    predictions = densenet_model.predict(test_drawing)
+
+    for i, prediction in enumerate(predictions):
+        top_3_predictions = prediction.argsort()[-3:][::-1]
+    return [categories[top_3_predictions[0]], categories[top_3_predictions[1]], categories[top_3_predictions[2]]]
+
+# print(predict_image())